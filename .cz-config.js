30723469ecdec6b62974846ec18ceae9f40ff939
ttian <ttian@vivo.com>
2020-03-17 09:42:14 +0800

    feat:new line

---
+++ .cz-config.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = {
+
+  types: [
+    {
+      value: 'feat',
+      name : 'a new feature.'
+    },
+    // ...
+  ]
+  scopes: [
+    // ...
+  ],
+
+  allowCustomScopes: true,
+  
+  allowBreakingChanges: [
+    'feat',
+    // ...
+  ],
+}
